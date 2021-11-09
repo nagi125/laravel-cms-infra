675dadb7ea3a41e8d16ad361a5c0bc69d287e0d1
nagi125 <nagi125.dev@gmail.com>
2021-11-09 19:11:40 +0900

    fix: elasticacheのvariableを分離

--- elasticache/main.tf
+++ elasticache/main.tf
@@ -1,15 +1,3 @@
-variable "app_name" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "private_subnet_ids" {
-  type = list(string)
-}
-
 locals {
   name = "${var.app_name}-redis"
 }
