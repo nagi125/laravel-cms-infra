3fb5a0b273307e7f0056752ab64fc7be51e9b89a
nagi125 <nagi125.dev@gmail.com>
2021-11-09 19:05:06 +0900

    fix: iamのvariableとoutputを分離

--- iam/main.tf
+++ iam/main.tf
@@ -1,7 +1,3 @@
-variable "app_name" {
-  type = string
-}
-
 resource "aws_iam_role" "task_execution" {
   name = "${var.app_name}-TaskExecution"
   assume_role_policy = file("./iam/task_execution_role.json")
@@ -16,8 +12,4 @@ resource "aws_iam_role_policy" "task_execution" {
 resource "aws_iam_role_policy_attachment" "task_execution" {
   role       = aws_iam_role.task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-output "iam_role_task_execution_arn" {
-  value = aws_iam_role.task_execution.arn
 }
---
+++ iam/output.tf
@@ -0,0 +1,3 @@
+output "iam_role_task_execution_arn" {
+  value = aws_iam_role.task_execution.arn
+}
